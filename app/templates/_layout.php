f85e09deaa32909e9945ebfa747bbc47e7e538f0
nwartell <50269968+nwartell@users.noreply.github.com>
2024-03-27 14:55:41 -0400

    CSS init

--- app/templates/_layout.php
+++ app/templates/_layout.php
@@ -2,6 +2,8 @@
 <html>
 
 <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <link href="css/main.css" rel="stylesheet"/>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <title><?= $title ?></title>
 </head>
